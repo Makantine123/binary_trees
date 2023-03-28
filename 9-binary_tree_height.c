ddb2de77203e7e2cf9a86be7a2ff6a2a109bf76c
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-28 12:31:17 +0200

    Commit

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,13 +11,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height = 0;
 
 	if (!tree)
-		return 0;
+		return (0);
 
 	left_height = binary_tree_height(tree->left) + 1;
 	right_height = binary_tree_height(tree->right) + 1;
-	
-	if (left_height > right_height)
-		return left_height;
 
-	return right_height;
+	if (left_height > right_height)
+		return (left_height);
+
+	return (right_height);
 }
