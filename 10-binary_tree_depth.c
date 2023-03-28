21a943821f67883ae074f3c85fb7009c0c2dcaea
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-28 12:45:45 +0200

    Commit

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - Function measures the depth of a node in a binary tree
+ * @tree: Pointer to a node to measure the depth
+ *
+ * Return: Depth size_t, else 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth;
+
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	depth = binary_tree_depth(tree->parent) + 1;
+
+	return (depth);
+}
