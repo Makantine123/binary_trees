52d1587c37450db33b11f8177f9448cd2fb56875
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-29 00:31:56 +0200

    Commit

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - Function counts the leaves in a binary tree
+ * @tree: Pointer to root node of tree to count
+ *
+ * Returns: Returns leave count, else 0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (!tree)
+		return (0);
+
+	leaves += binary_tree_size(tree->left);
+	leaves += binary_tree_size(tree->right);
+	/** Add root level to toatl**/
+	return (leaves);
+}
