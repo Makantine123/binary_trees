71781acce2ba0e04ae279d7e804e834b509b8ccd
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-28 23:22:09 +0200

    Commit

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,12 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t count = 1;
+	size_t num = 0;
 
 	if (!tree)
 		return (0);
-
-	count = binary_tree_size(tree->left) + 1;
-	count += binary_tree_size(tree->right);
+	num = binary_tree_size(tree->left) + 1;
+	num += binary_tree_size(tree->right);
 	/** Add root level to toatl**/
-	return (count);
+	return (num);
 }
