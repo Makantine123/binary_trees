54d5ccbf6ebf9e5ea0b2dbc3863b6961a118c104
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-29 02:03:54 +0200

    Commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left, right = 0;
+	int left, right = 0;
 
 	if (!tree)
 		return (0);
