450c4c18a8674550e65f1229343849e85cb94565
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-29 18:00:19 +0200

    Commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,8 +13,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree->right && tree->left)
 	{
-		if ((!tree->left->left &&!tree->left->right) &&
-			(!tree->right->left &&!tree->right->right))
+		if ((!tree->left->left && !tree->left->right) &&
+			(!tree->right->left && !tree->right->right))
 			return (1);
 
 		return (binary_tree_is_perfect(tree->left) &&
