45b4197cef891d9cee8d1ef4c988e6462dd5ea07
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-29 11:33:31 +0200

    Commit

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - Function checks if a binary tree is full
+ * @tree: Pointer to root node to check
+ *
+ * Return: 1 if tree is full else 0 if not or tree is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (1);
+
+	if ((tree->left == NULL) && (tree->right == NULL))
+		return (1);
+
+	if (!(tree->left) && !(tree->right))
+		return binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right);
+
+	return (0);
+}
