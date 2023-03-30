b6e18723711f2f34b7a4d44c71e1d24c1964a646
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-30 10:19:27 +0200

    Commit

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - Function finds the lowest commonelif
+ * ancestor of two nodes
+ * @first: Pointer to first node
+ * @second: Pointer to second node
+ *
+ * Return: Pointer to lowest common ancestor, else NULL if none found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *f_anc, *s_anc;
+
+	for (f_anc = first; f_anc; f_anc = f_anc->parent)
+	{
+		for (s_anc = second; s_anc; s_anc = s_anc->parent)
+		{
+			if (f_anc == s_anc)
+				return ((binary_tree_t *)f_anc);
+		}
+	}
+	return (NULL);
+}
