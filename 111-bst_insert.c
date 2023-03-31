3a326ed0e62f44e71aed0880576f2d835dd449fb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-31 02:05:18 +0200

    Commit

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,40 @@
 #include "binary_trees.h"
 /**
+ * bst_insert_recursion - Recursive transvers a Binary Search Tree 
+ * @tree: Pointer to tree node
+ * @value: Value to insert
+ *
+ * Return: Pointer to new node, NULL on failure
+ */
+bst_t *bst_insert_recursion(bst_t *tree, int value)
+{
+	bst_t *new = NULL;
+
+	if (tree->n > value)
+	{
+		if (tree->left == NULL)
+		{
+			new = binary_tree_node(tree, value);
+			tree->left = new;
+			return (new);
+		}
+		return (bst_insert_recursion(tree->left, value));
+	}
+	if (tree->n < value)
+	{
+		if (tree->right == NULL)
+		{
+			new = binary_tree_node(tree, value);
+			tree->right = new;
+			return (new);
+		}
+		return (bst_insert_recursion(tree->right, value));
+	}
+
+	return (NULL);
+}
+
+/**
  * bst_insert - Function inserts a value in Binary Search Tree
  * @tree: Double pointer to root node of BST to insert value
  * @value: Value to store in thr node to be inserted
@@ -8,7 +43,7 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *newNode, *temp;
+	bst_t *newNode;
 
 	if (tree == NULL)
 	{
@@ -16,35 +51,5 @@ bst_t *bst_insert(bst_t **tree, int value)
 		*tree = newNode;
 		return (newNode);
 	}
-
-	temp = *tree;
-
-	while (temp != NULL)
-	{
-		if (temp->n == value)
-		{
-			return (NULL);
-		}
-		if (temp->n > value)
-		{
-			if (temp->left == NULL)
-			{
-				temp->left = binary_tree_node(temp, value);
-				return (temp->left);
-			}
-			temp = temp->left;
-		}
-		if (temp->n < value)
-		{
-			if (temp->right == NULL)
-			{
-				temp->right = binary_tree_node(temp, value);
-				return (temp->right);
-			}
-			temp = temp->right;
-		}
-
-	}
-	return (NULL);
-
+	return (bst_insert_recursion(*tree, value));
 }
