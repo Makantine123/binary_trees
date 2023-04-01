de6ba4fa8407b28fe99500f1a928a6c9079ec36b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-01 20:46:53 +0200

    Commit

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -43,7 +43,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height, right_height, height_diff;
 
 	if (tree == NULL)
-		return (1);
+		return (0);
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
