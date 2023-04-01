df464a6504a33667c37aab33c3d16253ed3e3cad
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-01 08:04:31 +0200

    Commit

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -3,7 +3,6 @@
  * bst_remove - Function removes node from Binary Search Tree
  * @root: Pointer to root node tree
  * @value: Value to remove in the tree
- *
  * Return:Pointer to new root node of the tree after removing desired value
  */
 bst_t *bst_remove(bst_t *root, int value)
@@ -12,48 +11,33 @@ bst_t *bst_remove(bst_t *root, int value)
 
 	if (!root)
 		return (NULL);
-
 	if (value < root->n)
-	{
 		root->left = bst_remove(root->left, value);
 		if (root->left)
-		{
 			root->left->parent = root;
-		}
-	}
 	else if (value > root->n)
-	{
 		root->right = bst_remove(root->right, value);
 		if (root->right)
-		{
 			root->right->parent = root;
-		}
-	}
 	else
-	{
 		if (root->left == NULL)
-		{
 			temp = root->right;
 			free(root);
 			return (temp);
-		}
 		else if  (root->right == NULL)
-		{
 			temp = root->left;
 			free(root);
 			return (temp);
-		}
 		temp = bst_find_lowest(root->right);
 		root->n = temp->n;
 		root->right = bst_remove(root->right, temp->n);
 		if (root->right)
 			root->right->parent = root;
-	}
 	return (root);
 }
 
 /**
- * bst_find_low - Find lowest value in Binary Tree Search
+ * bst_find_lowest - Find lowest value in Binary Tree Search
  *
  * @tree: BST root or subtree
  * Return: Node to the tree containing lowest value
