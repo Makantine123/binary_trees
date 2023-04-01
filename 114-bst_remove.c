4b4c624839524465e020529930d4a2056852106a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-01 08:52:08 +0200

    Commit

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -12,35 +12,43 @@ bst_t *bst_remove(bst_t *root, int value)
 	if (!root)
 		return (NULL);
 	if (value < root->n)
+	{
 		root->left = bst_remove(root->left, value);
-	if (root->left)
-		root->left->parent = root;
+		if (root->left)
+			root->left->parent = root;
+	}
 	else if (value > root->n)
+	{
 		root->right = bst_remove(root->right, value);
 		if (root->right)
 			root->right->parent = root;
-		else
-			if (root->left == NULL)
-				temp = root->right;
-				free(root);
-				return (temp);
-			else if  (root->right == NULL)
-			{
-				temp = root->left;
-				free(root);
-				return (temp);
-			}
-			temp = bst_find_lowest(root->right);
-			root->n = temp->n;
-			root->right = bst_remove(root->right, temp->n);
-			if (root->right)
-				root->right->parent = root;
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			temp = root->right;
+			free(root);
+			return (temp);
+		}
+		else if  (root->right == NULL)
+		{
+			temp = root->left;
+			free(root);
+			return (temp);
+		}
+
+		temp = bst_find_lowest(root->right);
+		root->n = temp->n;
+		root->right = bst_remove(root->right, temp->n);
+		if (root->right)
+			root->right->parent = root;
+	}
 	return (root);
 }
 
 /**
  * bst_find_lowest - Find lowest value in Binary Tree Search
- *
  * @tree: BST root or subtree
  * Return: Node to the tree containing lowest value
  */
@@ -49,8 +57,11 @@ bst_t *bst_find_lowest(bst_t *tree)
 	bst_t *temp = NULL;
 
 	temp = tree;
+
 	while (temp && temp->left)
+	{
 		temp = temp->left;
+	}
 
 	return (temp);
 }
