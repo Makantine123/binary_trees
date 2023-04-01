4825f7d151a197b2f50a880b7214eaf8da15bf60
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-04-01 08:27:35 +0200

    Commit

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -13,26 +13,28 @@ bst_t *bst_remove(bst_t *root, int value)
 		return (NULL);
 	if (value < root->n)
 		root->left = bst_remove(root->left, value);
-		if (root->left)
-			root->left->parent = root;
+	if (root->left)
+		root->left->parent = root;
 	else if (value > root->n)
 		root->right = bst_remove(root->right, value);
 		if (root->right)
 			root->right->parent = root;
-	else
-		if (root->left == NULL)
-			temp = root->right;
-			free(root);
-			return (temp);
-		else if  (root->right == NULL)
-			temp = root->left;
-			free(root);
-			return (temp);
-		temp = bst_find_lowest(root->right);
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
-		if (root->right)
-			root->right->parent = root;
+		else
+			if (root->left == NULL)
+				temp = root->right;
+				free(root);
+				return (temp);
+			else if  (root->right == NULL)
+			{
+				temp = root->left;
+				free(root);
+				return (temp);
+			}
+			temp = bst_find_lowest(root->right);
+			root->n = temp->n;
+			root->right = bst_remove(root->right, temp->n);
+			if (root->right)
+				root->right->parent = root;
 	return (root);
 }
 
